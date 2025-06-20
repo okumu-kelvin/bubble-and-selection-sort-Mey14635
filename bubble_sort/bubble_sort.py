8c45e4fbe220c9c1a160ce9f1b0a766ee694af53
Mey14635 <kasomediatrice1407@gmail.com>
2025-06-20 13:55:09 +0300

    submission commit

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,19 @@
 def bubble_sort(unsorted_list):
     # TODO: Implement bubble sort
+//the first loop is to check how many time will go through the array
+        for i in range(len(unsorted_list)-1,0,-1)
+//the second loop is to do the comparison then swap them
+            for j in range(i):
+                //To do the swap we add a temporary variable to store the variable
+                if unsorted_list[j]>unsorted_list[j+1]:
+                    temp=unsorted_list[j]
+                    unsorted_list[j]=unsorted_list[j+1]
+                    unsorted_list[j+1]=temp
+        return unsorted_list
+
+nums=[5, 4, 3, 2, 1, 0]
+bubble_sort(nums)
+print(nums)
+
+
     pass
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,18 @@
 def selection_sort(arr):
     # TODO: Implement selection sort
+for i in range(n):
+    minpos=i
+//the first loop go through each index i in the list by not the last one
+    for j in range(len(arr)-1):
+// minpos will track the index of the smallest value found in the remaining list
+        if arr[j]<arr[minpos]:
+            minpos=j
+     temp=arr[i]
+     arr[i]=arr[minpos]
+     arr[minpos]=temp
+ arr=[5, 4, 3, 2, 1, 0]
+ selection_sort(arr)
+ print(arr)
+
+ return arr
     pass
