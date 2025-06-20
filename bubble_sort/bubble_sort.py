707c936e8e5151abd10aca644af806e843ed8432
Mey14635 <kasomediatrice1407@gmail.com>
2025-06-20 14:32:12 +0300

    Update bubble_sort.py

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,10 +1,10 @@
 def bubble_sort(unsorted_list):
     # TODO: Implement bubble sort
-//the first loop is to check how many time will go through the array
+#the first loop is to check how many time will go through the array
         for i in range(len(unsorted_list)-1,0,-1):
-//the second loop is to do the comparison then swap them
+#the second loop is to do the comparison then swap them
             for j in range(i):
-                //To do the swap we add a temporary variable to store the variable
+                #To do the swap we add a temporary variable to store the variable
                 if unsorted_list[j]>unsorted_list[j+1]:
                     temp=unsorted_list[j]
                     unsorted_list[j]=unsorted_list[j+1]
